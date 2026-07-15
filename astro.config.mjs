8c528ae14a30486dbfe88fad900d8b3082cf592c
moo <moo@moodeMacBook-Pro.local>
2026-07-15 17:44:08 +0800

    Revert "feat: add custom domain moo7818.cc.cd"
    This reverts commit c39a27f7990ef92cc90945a1e658f5ab5b298819.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,8 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 
 export default defineConfig({
-  site: 'https://moo7818.cc.cd',
+  site: 'https://moo7818.github.io',
+  base: '/moo-log/',
   build: {
     format: 'directory',
   },
